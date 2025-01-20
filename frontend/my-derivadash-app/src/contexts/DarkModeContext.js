5424a6d2ef92f8ebce10da6e1c85a66ccf772a0d
jxc2008 <jxc2008@nyu.edu>
2025-01-19 19:45:36 -0600

    reverted3

--- frontend/my-derivadash-app/src/contexts/DarkModeContext.js
+++ frontend/my-derivadash-app/src/contexts/DarkModeContext.js
@@ -1,29 +1,32 @@
-import React, { createContext, useState, useContext, useEffect } from "react"
+import React, { createContext, useState, useContext, useEffect } from "react";
 
-const DarkModeContext = createContext()
+const DarkModeContext = createContext();
 
 export const DarkModeProvider = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
-    const isDarkMode = localStorage.getItem("darkMode") === "true"
-    setDarkMode(isDarkMode)
-  }, [])
+    const isDarkMode = localStorage.getItem("darkMode") === "true";
+    setDarkMode(isDarkMode);
+  }, []);
 
   const toggleDarkMode = () => {
-    const newDarkMode = !darkMode
-    setDarkMode(newDarkMode)
-    localStorage.setItem("darkMode", newDarkMode.toString())
-  }
+    const newDarkMode = !darkMode;
+    setDarkMode(newDarkMode);
+    localStorage.setItem("darkMode", newDarkMode.toString());
+  };
 
-  return <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>{children}</DarkModeContext.Provider>
-}
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
 
 export const useDarkMode = () => {
-  const context = useContext(DarkModeContext)
+  const context = useContext(DarkModeContext);
   if (context === undefined) {
-    throw new Error("useDarkMode must be used within a DarkModeProvider")
+    throw new Error("useDarkMode must be used within a DarkModeProvider");
   }
-  return context
-}
-
+  return context;
+};
