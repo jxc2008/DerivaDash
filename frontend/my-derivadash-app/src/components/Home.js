32a02ed1985adf396197d569ad0e3e457919cf58
jxc2008 <jxc2008@nyu.edu>
2025-01-19 18:28:47 -0600

    fixed

--- frontend/my-derivadash-app/src/components/Home.js
+++ frontend/my-derivadash-app/src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { useNavigate, Link } from "react-router-dom"
-import "./App.css"
+import "../App.css"
 import "./Home.css"
 import { useDarkMode } from "../contexts/DarkModeContext"
 
